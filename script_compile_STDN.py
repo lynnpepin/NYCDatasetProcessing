3e01237f26e097abbdeda0dadfd3c23fecfd7260
tpepin96 <tpepin96@gmail.com>
2018-07-17 15:15:52 -0400

    Finished up additional data processing scripts

---
+++ script_compile_STDN.py
@@ -0,0 +1,134 @@
+import numpy as np
+from utils import generate_dates
+
+''' Use this to take all the individual STDN files in data/
+    and put them into four large numpy arrays.
+    
+    This is split first by flow/volume, and second by the
+    break in data (no data from 2011-06 to 2011-09).
+'''
+
+
+# First batch of filenames
+fns_f_1 =  ["STDN-flow-2010-01.npz",
+            "STDN-flow-2010-02.npz",
+            "STDN-flow-2010-03.npz",
+            "STDN-flow-2010-04.npz",
+            "STDN-flow-2010-05.npz",
+            "STDN-flow-2010-06.npz",
+            "STDN-flow-2010-07.npz",
+            "STDN-flow-2010-08.npz",
+            "STDN-flow-2010-09.npz",
+            "STDN-flow-2010-10.npz",
+            "STDN-flow-2010-11.npz",
+            "STDN-flow-2010-12.npz",
+            "STDN-flow-2011-01.npz",
+            "STDN-flow-2011-02.npz",
+            "STDN-flow-2011-03.npz",
+            "STDN-flow-2011-04.npz",
+            "STDN-flow-2011-05.npz"]
+
+fns_v_1 =  ["STDN-volume-2010-01.npz",
+            "STDN-volume-2010-02.npz",
+            "STDN-volume-2010-03.npz",
+            "STDN-volume-2010-04.npz",
+            "STDN-volume-2010-05.npz",
+            "STDN-volume-2010-06.npz",
+            "STDN-volume-2010-07.npz",
+            "STDN-volume-2010-08.npz",
+            "STDN-volume-2010-09.npz",
+            "STDN-volume-2010-10.npz",
+            "STDN-volume-2010-11.npz",
+            "STDN-volume-2010-12.npz",
+            "STDN-volume-2011-01.npz",
+            "STDN-volume-2011-02.npz",
+            "STDN-volume-2011-03.npz",
+            "STDN-volume-2011-04.npz",
+            "STDN-volume-2011-05.npz"]
+
+# Second batch of filenames
+fns_f_2 = [ "STDN-flow-2011-10.npz",
+            "STDN-flow-2011-11.npz",
+            "STDN-flow-2011-12.npz",
+            "STDN-flow-2012-01.npz",
+            "STDN-flow-2012-02.npz",
+            "STDN-flow-2012-03.npz",
+            "STDN-flow-2012-04.npz",
+            "STDN-flow-2012-05.npz",
+            "STDN-flow-2012-06.npz",
+            "STDN-flow-2012-07.npz",
+            "STDN-flow-2012-08.npz",
+            "STDN-flow-2012-09.npz",
+            "STDN-flow-2012-10.npz",
+            "STDN-flow-2012-11.npz",
+            "STDN-flow-2012-12.npz",
+            "STDN-flow-2013-01.npz",
+            "STDN-flow-2013-02.npz",
+            "STDN-flow-2013-03.npz",
+            "STDN-flow-2013-04.npz",
+            "STDN-flow-2013-05.npz",
+            "STDN-flow-2013-06.npz",
+            "STDN-flow-2013-07.npz",
+            "STDN-flow-2013-08.npz",
+            "STDN-flow-2013-09.npz",
+            "STDN-flow-2013-10.npz",
+            "STDN-flow-2013-11.npz",
+            "STDN-flow-2013-12.npz"]
+
+fns_v_2  = ["STDN-volume-2011-10.npz",
+            "STDN-volume-2011-11.npz",
+            "STDN-volume-2011-12.npz",
+            "STDN-volume-2012-01.npz",
+            "STDN-volume-2012-02.npz",
+            "STDN-volume-2012-03.npz",
+            "STDN-volume-2012-04.npz",
+            "STDN-volume-2012-05.npz",
+            "STDN-volume-2012-06.npz",
+            "STDN-volume-2012-07.npz",
+            "STDN-volume-2012-08.npz",
+            "STDN-volume-2012-09.npz",
+            "STDN-volume-2012-10.npz",
+            "STDN-volume-2012-11.npz",
+            "STDN-volume-2012-12.npz",
+            "STDN-volume-2013-01.npz",
+            "STDN-volume-2013-02.npz",
+            "STDN-volume-2013-03.npz",
+            "STDN-volume-2013-04.npz",
+            "STDN-volume-2013-05.npz",
+            "STDN-volume-2013-06.npz",
+            "STDN-volume-2013-07.npz",
+            "STDN-volume-2013-08.npz",
+            "STDN-volume-2013-09.npz",
+            "STDN-volume-2013-10.npz",
+            "STDN-volume-2013-11.npz",
+            "STDN-volume-2013-12.npz"]
+
+for ii, pair in enumerate(((fns_f_1, fns_v_1), (fns_f_2, fns_v_2))):
+    fns_f = pair[0]
+    fns_v = pair[1]
+
+    # fdata
+    # Load from the first line
+    fdata = np.load("data/" + fns_f[0])['arr_0']
+
+    # And then concate from the rest of the lines
+    for fname in fns_f[1:]:
+        print("Loading from",fname)
+        fdata = np.concatenate((fdata, np.load("data/" + fname)['arr_0']), axis=1)
+
+    print("Saving to STDN-flow-"+str(ii+1)+".npz")
+    np.savez_compressed("data/STDN-flow-"+str(ii+1)+".npz",fdata)
+    del(fdata) # Free from memory!
+
+    # vdata
+    # Load from the first line
+    print("Loading from", fns_v[0])
+    vdata = np.load("data/" + fns_v[0])['arr_0']
+    # And then concate from the rest of the lines
+    for fname in fns_v[1:]:
+        print("Loading from", fname)
+        vdata = np.concatenate((vdata, np.load("data/" + fname)['arr_0']), axis=0)
+
+    print("Saving to STDN-volume-"+str(ii+1)+".npz")
+    np.savez_compressed("data/STDN-volume-"+str(ii+1)+".npz",vdata)
+    del(vdata) # Free from memory!
--- script_data_to_stdn.py
+++ script_data_to_stdn.py
@@ -1,8 +1,77 @@
 import numpy as np
 from utils import generate_dates
 
-for (year, month) in generate_dates(
+'''
+After being run through the data processor, use this script to:
+    1. Reduce the time resolution via summation, from 4 slots per hour to 2
+    2. Remove the pcount/tcount axis, looking only at the trip count.
+This assumes all data is in a /data folder.
 
-data = np.load("2010-01-data.npz")
-vdata = data['vdata'][:,:,:,:,1]
-fdata = data['fdata'][:,:,:,:,:,:,1]
+Use 'script_compile_STDN' to further compile this data.
+'''
+
+fnames = ["2010-01-data.npz",
+          "2010-02-data.npz",
+          "2010-03-data.npz",
+          "2010-04-data.npz",
+          "2010-05-data.npz",
+          "2010-06-data.npz",
+          "2010-07-data.npz",
+          "2010-08-data.npz",
+          "2010-09-data.npz",
+          "2010-10-data.npz",
+          "2010-11-data.npz",
+          "2010-12-data.npz",
+          "2011-01-data.npz",
+          "2011-02-data.npz",
+          "2011-03-data.npz",
+          "2011-04-data.npz",
+          "2011-05-data.npz",
+          #2011-06 to 2011-09 does not have data!
+          "2011-10-data.npz",
+          "2011-11-data.npz",
+          "2011-12-data.npz",
+          "2012-01-data.npz",
+          "2012-02-data.npz",
+          "2012-03-data.npz",
+          "2012-04-data.npz",
+          "2012-05-data.npz",
+          "2012-06-data.npz",
+          "2012-07-data.npz",
+          "2012-08-data.npz",
+          "2012-09-data.npz",
+          "2012-10-data.npz",
+          "2012-11-data.npz",
+          "2012-12-data.npz",
+          "2013-01-data.npz",
+          "2013-02-data.npz",
+          "2013-03-data.npz",
+          "2013-04-data.npz",
+          "2013-05-data.npz",
+          "2013-06-data.npz",
+          "2013-07-data.npz",
+          "2013-08-data.npz",
+          "2013-09-data.npz",
+          "2013-10-data.npz",
+          "2013-11-data.npz",
+          "2013-12-data.npz"]
+
+
+for fname in fnames:
+    datestr = fname[0:7]
+    data = np.load("data/"+fname)
+
+    vdata = data['vdata'][:,:,:,:,1]
+    fdata = data['fdata'][:,:,:,:,:,:,1]
+    
+    # Assuming vdata and fdata have the same size/resolution!!
+    samples = vdata.shape[0]
+    w = vdata.shape[1]
+    h = vdata.shape[2]
+    n = 2
+
+    vdata = vdata.reshape(int(samples/n), n, w, h, 2).sum(axis=1)
+    fdata = fdata.reshape(2, int(samples/n), n, w, h, w, h).sum(axis=2)
+
+    np.savez_compressed("data/STDN-volume-"+datestr+".npz", vdata)
+    np.savez_compressed("data/STDN-flow-"+datestr+".npz", fdata)
